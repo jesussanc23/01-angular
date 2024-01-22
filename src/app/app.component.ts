77d6ee04faab160d9c4a93f16e0b8bb4ffbbe373
Jesus Sanchez <jesus_sanchez@epam.com>
2024-01-22 09:41:43 -0600

    Pokemons json loaded from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import DataJson from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
@@ -12,36 +13,5 @@ import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 })
 export class AppComponent {
   title = '01-angular';
-  pokemons = [
-      {
-        "name": "bulbasaur",
-        "id": 1,
-        "forms_switchable": false
-      },
-      {
-        "name": "ivysaur",
-        "id": 2,
-        "forms_switchable": false
-      },
-      {
-        "name": "venusaur",
-        "id": 3,
-        "forms_switchable": true
-      },
-      {
-        "name": "charmander",
-        "id": 4,
-        "forms_switchable": false
-      },
-      {
-        "name": "charmeleon",
-        "id": 5,
-        "forms_switchable": false
-      },
-      {
-        "name": "charizard",
-        "id": 6,
-        "forms_switchable": true
-      }
-  ];
+  pokemons = DataJson;
 }
